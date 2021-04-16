76e721e26757f231b086595666eabd951c757ab1
Onkar <er.onkarw@gmail.com>
2021-04-16 23:24:44 +0530

    fully resonsive webpage

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+function handleClick(arg){
+    
+    if(arg===1){
+        document.getElementById("displayContent").innerHTML="In the end, we only regret the chances we didn’t take";
+    }else if(arg===2){
+        document.getElementById("displayContent").innerHTML="The journey of a thousand miles begins with a single step";
+    }else{
+        document.getElementById("displayContent").innerHTML="Have stories to tell not stuff to show.";
+    }
+}
